ab4bb503c745c39dfefa9db79a1574666e14daea
Tejas Vedagiri <tejasvedagiri1223@gmail.com>
2022-11-28 19:39:16 -0500

    Updated logic of file processing

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 from sklearn.linear_model import LogisticRegression
 
-SAMPLE_DATA = 100
+SAMPLE_DATA = 5
 
 torch.manual_seed(42)
 df = pd.read_csv("creditcard.csv")
@@ -32,23 +32,31 @@ lgr.fit(X_train, y_train)
 y_pred = lgr.predict(X_test)
 y_pred_faulty = lgr.predict(faulty_transactions_x)
 
+
 score_dict["base_line_lgr"] = {"acc_all_data": accuracy_score(y_test, y_pred), "f1_all_data": f1_score(y_test, y_pred, average='macro'),
                                 "acc_faulty_data": accuracy_score(faulty_y, y_pred_faulty), "f1_faulty_data": f1_score(faulty_y, y_pred_faulty, average='macro')}
 
+for run in range(0,100):
+    failed_index = np.where(y_pred_faulty == 0)
+    random_index = np.random.choice(failed_index[0], SAMPLE_DATA)
+    noise_data = faulty_transactions_x[random_index]
+    
+    for model_path in os.listdir("model"):
+        model = torch.load(os.path.join("model",model_path)).cpu()
 
-for model_path in os.listdir("model"):
-    model = torch.load(os.path.join("model",model_path)).cpu()
-    new_error = model(torch.rand(SAMPLE_DATA, 28).cpu())
-    new_Y = np.ones((SAMPLE_DATA,1))
+        
+        #new_error = model(torch.rand(SAMPLE_DATA, 28).cpu())
+        new_error = model(torch.tensor(noise_data).float().cpu())
+        new_Y = np.ones((SAMPLE_DATA,1))
 
-    X_train = np.vstack((X_train, new_error[0].detach().numpy()))
-    y_train = np.vstack((y_train.reshape(-1,1), new_Y))
+        X_train = np.vstack((X_train, new_error[0].detach().numpy()))
+        y_train = np.vstack((y_train.reshape(-1,1), new_Y))
 
 
-    lgr = LogisticRegression(random_state=42)
-    lgr.fit(X_train, y_train)
-    y_pred = lgr.predict(X_test)
-    y_pred_faulty = lgr.predict(faulty_transactions_x)
+        lgr = LogisticRegression(random_state=42)
+        lgr.fit(X_train, y_train)
+        y_pred = lgr.predict(X_test)
+        y_pred_faulty = lgr.predict(faulty_transactions_x)
 
     score_dict[model_path+"_lgr"] = {"acc_all_data": accuracy_score(y_test, y_pred), "f1_all_data": f1_score(y_test, y_pred, average='macro'),
                                 "acc_faulty_data": accuracy_score(faulty_y, y_pred_faulty), "f1_faulty_data": f1_score(faulty_y, y_pred_faulty, average='macro')}
